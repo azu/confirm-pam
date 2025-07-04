1460eadb5d76b35296e1305970db84323ad120e3
azu <azuciao@gmail.com>
2025-07-04 10:58:03 +0900

    update: add actual SHA256 hash to Homebrew formula

--- Formula/confirm-pam.rb
+++ Formula/confirm-pam.rb
@@ -2,7 +2,7 @@ class ConfirmPam < Formula
   desc "A CLI tool for biometric authentication confirmation"
   homepage "https://github.com/azu/confirm-pam"
   url "https://github.com/azu/confirm-pam/releases/download/v0.1.0/confirm-pam-macos-amd64.tar.gz"
-  sha256 "PLACEHOLDER_SHA256_HASH"
+  sha256 "636c9cb66c3c5244ad318e4cf9516f04713fd1ee946cac2cc8ed16bf4775c32f"
   license "MIT"
 
   def install
