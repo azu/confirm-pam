42b0d1ca940735ec147d5fa7261583ec7ac6ac58
azu <azuciao@gmail.com>
2025-07-04 10:54:01 +0900

    fix: add toolchain parameter for rust-toolchain action and add homebrew formula

---
+++ Formula/confirm-pam.rb
@@ -0,0 +1,15 @@
+class ConfirmPam < Formula
+  desc "A CLI tool for biometric authentication confirmation"
+  homepage "https://github.com/azu/confirm-pam"
+  url "https://github.com/azu/confirm-pam/releases/download/v0.1.0/confirm-pam-macos-amd64.tar.gz"
+  sha256 "PLACEHOLDER_SHA256_HASH"
+  license "MIT"
+
+  def install
+    bin.install "confirm-pam"
+  end
+
+  test do
+    assert_match "confirm-pam", shell_output("#{bin}/confirm-pam --help")
+  end
+end
